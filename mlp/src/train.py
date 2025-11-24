6ce75d816cc2a81686bf1a5412dd6553cdfa9bb4
Vyom Thakkar <vyom.thakkar3508@gmail.com>
2025-11-24 23:36:30 +0530

    train.py experimenting with model output chainining from emb -> w1

--- mlp/src/train.py
+++ mlp/src/train.py
@@ -18,7 +18,9 @@ lossi = []
 print(f"Xtr shape: {setup.Xtr.shape}")
 emb = model.C[setup.Xtr]
 print(emb.shape)
-    
+print(f"W1.shape: {model.W1.shape}")
+o1 = emb.view(emb.shape[0], -1) @ model.W1 + model.b1
+print(f"o1.shape: {o1.shape}")
     
     
     
