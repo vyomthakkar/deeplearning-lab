8ae82cf1d08f7978d0464afe1c79788aa2e1e410
Vyom Thakkar <vyom.thakkar3508@gmail.com>
2025-11-25 20:43:23 +0530

    forward pass chained

--- mlp/src/train.py
+++ mlp/src/train.py
@@ -1,6 +1,7 @@
 import torch
 import model
 import setup
+import torch.nn.functional as F
 
 max_steps = 200000
 batch_size = 32
@@ -15,12 +16,20 @@ lossi = []
     
     
     
-print(f"Xtr shape: {setup.Xtr.shape}")
+print(f"{setup.Xtr.shape}")
 emb = model.C[setup.Xtr]
 print(emb.shape)
-print(f"W1.shape: {model.W1.shape}")
+print(f"{model.W1.shape=}")
 o1 = emb.view(emb.shape[0], -1) @ model.W1 + model.b1
-print(f"o1.shape: {o1.shape}")
+print(f"{o1.shape=}")
+o2 = o1 @ model.W2 + model.b2
+print(f"{o2.shape=}")
+
+loss = F.cross_entropy(o2, setup.Ytr)
+print(f"{loss=}")
+
+
+
     
     
     
