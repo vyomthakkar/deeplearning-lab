76b07b2f3ae390dcf79f8d8079cd2b045e52d6c6
Vyom Thakkar <vyom.thakkar3508@gmail.com>
2025-11-25 23:12:45 +0530

    added tanh nl after preactivation in test forward pass chain

--- mlp/src/setup.py
+++ mlp/src/setup.py
@@ -49,3 +49,4 @@ print(Xtest.shape, Ytest.shape)
 
 
 
+
--- mlp/src/train.py
+++ mlp/src/train.py
@@ -20,13 +20,15 @@ print(f"{setup.Xtr.shape}")
 emb = model.C[setup.Xtr]
 print(emb.shape)
 print(f"{model.W1.shape=}")
-o1 = emb.view(emb.shape[0], -1) @ model.W1 + model.b1
-print(f"{o1.shape=}")
-o2 = o1 @ model.W2 + model.b2
-print(f"{o2.shape=}")
+hpreact = emb.view(emb.shape[0], -1) @ model.W1 + model.b1
+h = torch.tanh(hpreact)
+print(f"{h.shape=}")
+logits = h @ model.W2 + model.b2
+print(f"{logits.shape=}")
 
-loss = F.cross_entropy(o2, setup.Ytr)
+loss = F.cross_entropy(logits, setup.Ytr)
 print(f"{loss=}")
+
 
 
 
