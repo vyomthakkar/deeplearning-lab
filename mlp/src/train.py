13670c9eac290d0d2575fac1e35c1661e7f55c64
Vyom Thakkar <vyom.thakkar3508@gmail.com>
2025-11-27 23:18:07 +0530

    cleaned up training loop code, need to fix slownes

--- mlp/src/train.py
+++ mlp/src/train.py
@@ -5,41 +5,40 @@ import torch.nn.functional as F
 
 max_steps = 200000
 batch_size = 32
-lr = 1e-2
 lossi = []
 
-
-# for i in range(max_steps):
-#     #forward pass
-#     emb = C[Xtr]
     
 for i in range(max_steps):
     ix = torch.randint(0, setup.Xtr.shape[0], (batch_size,))
     Xb, Yb = setup.Xtr[ix], setup.Ytr[ix]    
     
-    # print(f"{setup.Xtr.shape}")
+    #forward pass
     emb = model.C[setup.Xtr]
-    # print(emb.shape)
-    # print(f"{model.W1.shape=}")
-    hpreact = emb.view(emb.shape[0], -1) @ model.W1 + model.b1
+    embcat = emb.view(emb.shape[0], -1)
+    hpreact = embcat @ model.W1 + model.b1
     h = torch.tanh(hpreact)
-    # print(f"{h.shape=}")
     logits = h @ model.W2 + model.b2
-    # print(f"{logits.shape=}")
 
+    #compute loss
     loss = F.cross_entropy(logits, setup.Ytr)
-    # print(f"{loss=}")
     
+    #zero out gradients
     for p in model.parameters:
         p.grad = None
+    
+    #backpropagate via loss and accumulate gradients
     loss.backward()
     
+    #update parameters
+    lr = 0.1 if i < 100000 else 0.01
     for p in model.parameters:
-        p.data += -lr * p.grad 
+        p.data += -lr * p.grad
         
-    if i % 100 == 0:
+    if i % 10000 == 0:
         print(f"{i=}/{max_steps=} {loss=}")
-
+    lossi.append(loss.item())
+        
+    
 
 
 
